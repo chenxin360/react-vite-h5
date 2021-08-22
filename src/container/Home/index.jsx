39b738b62772a2aae6cfca95daf66f502e9f6fa4
chenxin <chenxin_web@163.com>
2021-08-22 23:55:08 +0800

    账单页提交

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -6,9 +6,26 @@
  * @FilePath: \react-vite-h5\src\container\Home\index.jsx
  */
 import React from 'react'
+import {Icon} from "zarm";
+import style from './style.module.less'
 
 const Home = () => {
-    return <div>首页</div>
+    return <div className={style.home}>
+        <div className={style.header}>
+            <div className={style.dataWrap}>
+                <span className={style.expense}>总支出: <b>￥ 200</b></span>
+                <span className={style.income}>总收入: <b>￥ 500</b></span>
+            </div>
+            <div className={style.typeWrap}>
+                <div className={style.left}>
+                    <span className={style.title}>类型 <Icon className={style.arrow} type="arrow-bottom"/></span>
+                </div>
+                <div className={style.right}>
+                    <span className={style.time}>2022-06<Icon className={style.arrow} type="arrow-bottom"/></span>
+                </div>
+            </div>
+        </div>
+    </div>
 }
 
 export default Home
